373c3541135ac4c877ccc1bb5d486419abc121fc
rsu <rsu@encode.dk>
2016-01-14 18:22:20 +0200

    Step 3 - bower

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 requirejs.config({
 	baseUrl: '/frontend-deploy-workshop.git/src',
 	paths: {
-		text:"./libs/text",
-		jquery:"./libs/jquery.min",
-		underscore:"./libs/underscore-min",
-		backbone:"./libs/backbone-min"
+		text:"./libs/text/text",
+		jquery:"./libs/jquery/dist/jquery.min",
+		underscore:"./libs/underscore/underscore-min",
+		backbone:"./libs/backbone/backbone-min"
 	}
 });
 require(["./src/js/app.js"]);
